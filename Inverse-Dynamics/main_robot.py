76690cc7481e4d315582fbf3abdacb8f8642986b
Thomas Flayols <thomas.flayols@gmail.com>
2020-01-21 13:37:49 +0100

    Split the main sc ript into 2 scripts : one for the experimentation main_robot.py, the other for the simulation main_simu.py

---
+++ Inverse-Dynamics/main_robot.py
@@ -0,0 +1,142 @@
+# coding: utf8
+
+import numpy as np 
+import time
+import libmaster_board_sdk_pywrap as mbs
+
+# import the controller class with its parameters
+from P_controller import controller, dt, q0, omega
+import Relief_controller
+import EmergencyStop_controller
+
+
+def example_script(name_interface):
+	
+    ########################################################################
+    #                Parameters definition & Initialization                #
+    ########################################################################
+
+    # Simulation parameters
+    N_SIMULATION = 10000	# number of time steps simulated
+    t = 0.0  				# time
+    # Initialize the error for the simulation time
+    time_error = False
+    
+    N_SLAVES = 6  # Maximum number of controled drivers
+	N_SLAVES_CONTROLED = 2  # Current number of controled drivers
+
+	iq_sat = 1.0  # Maximum amperage (A)
+	init_pos = [0.0 for i in range(N_SLAVES * 2)]  # List that will store the initial position of motors
+	state = 0  # State of the system (ready (1) or not (0))
+
+	robot_if = mbs.MasterBoardInterface(name_interface) # Robot interface
+	robot_if.Init()  # Initialization of the interface between the computer and the master board
+	
+	for i in range(N_SLAVES_CONTROLED):  # We enable each controler driver and its two associated motors
+		robot_if.GetDriver(i).motor1.SetCurrentReference(0)
+		robot_if.GetDriver(i).motor2.SetCurrentReference(0)
+		robot_if.GetDriver(i).motor1.Enable()
+		robot_if.GetDriver(i).motor2.Enable()
+		robot_if.GetDriver(i).EnablePositionRolloverError()
+		robot_if.GetDriver(i).SetTimeout(5)
+		robot_if.GetDriver(i).Enable()
+
+	qmes = np.zeros((8,1))
+	vmes = np.zeros((8,1))
+	K = 1.0		# proportionnal gain to convert torques to current	
+	
+    print("-- Start of example script --")
+
+    ########################################################################
+    #                            Control loop                              #
+    ########################################################################
+
+    myController = controller(q0, omega, t)
+    myReliefController = Relief_controller.controller()
+    myEmergencyStop = EmergencyStop_controller.controller()
+
+
+    for i in range (N_SIMULATION):
+        
+        time_start = time.time()
+        
+        robot_if.ParseSensorData()  # Read sensor data sent by the masterboard
+
+        if (state == 0):  # If the system is not ready
+            state = 1
+            for i in range(N_SLAVES_CONTROLED * 2):  # Check if all motors are enabled and ready
+                if not (robot_if.GetMotor(i).IsEnabled() and robot_if.GetMotor(i).IsReady()):
+                    state = 0
+                init_pos[i] = robot_if.GetMotor(i).GetPosition()
+                t = 0.0
+        else:  # If the system is ready
+            for i in range(N_SLAVES_CONTROLED * 2):
+                if robot_if.GetMotor(i).IsEnabled():
+                    qmes[i] = robot_if.GetMotor(i).GetPosition()
+                    vmes[i] = robot_if.GetMotor(i).GetVelocity()
+
+
+        ####################################################################
+        #                Select the appropriate controller 				   #
+        #								&								   #
+        #				Load the joint torques into the robot			   #
+        ####################################################################
+        
+        # If the limit bounds are reached, controller is switched to a pure derivative controller
+        if(myController.error):
+            print("Safety bounds reached. Switch to a safety controller")
+            myController = myReliefController
+            
+        # If the simulation time is too long, controller is switched to a zero torques controller
+        time_error = time_error or (time.time()-time_start > 0.003)
+        if (time_error):
+            print("Computation time lasted to long. Switch to a zero torque control")
+            myController = myEmergencyStop
+            
+        # Retrieve the joint torques from the appropriate controller
+        jointTorques = myController.control(qmes, vmes, t)
+        
+        # Time incrementation
+        t += dt
+        
+        if (state==1):
+            for i in range(N_SLAVES_CONTROLED * 2):
+                if robot_if.GetMotor(i).IsEnabled():
+                    cur = K * jointTorques
+                    if (cur > iq_sat):  # Check saturation
+                        cur = iq_sat
+                    if (cur < -iq_sat):
+                        cur = -iq_sat
+                    robot_if.GetMotor(i).SetCurrentReference(cur)  # Set reference currents
+        robot_if.SendCommand()  # Send the reference currents to the master board
+    
+    # At the end of the loop, set the reference current to zero
+    for i in range(N_SLAVES_CONTROLED * 2):
+        if robot_if.GetMotor(i).IsEnabled():
+            robot_if.GetMotor(i).SetCurrentReference(0.0)  # Set reference currents
+    robot_if.SendCommand()
+    
+    robot_if.Stop()  # Shut down the interface between the computer and the master board
+    
+    print("-- End of example script --")
+
+
+def main(argv):
+    name_interface = ""  # Name of the interface (use ifconfig in a terminal), for instance "enp1s0"
+    try:
+        opts, args = getopt.getopt(argv, "hi:")
+    except getopt.GetoptError:
+        print("example.py -i <interface>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("example.py -i <interface>")
+            sys.exit()
+        elif opt in ("-i") and isinstance(arg, str):
+            name_interface = arg
+    print("name_interface: ", name_interface)
+    example_script(name_interface)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ Inverse-Dynamics/main_simu.py
@@ -0,0 +1,138 @@
+# coding: utf8
+
+import numpy as np 
+import time
+import pybullet as p
+import pybullet_data
+import matplotlib.pylab as plt
+
+# import the controller class with its parameters
+from P_controller import controller, dt, q0, omega
+import Relief_controller
+import EmergencyStop_controller
+	
+
+########################################################################
+#                        Parameters definition                         #
+########################################################################
+	
+# Simulation parameters
+N_SIMULATION = 10000	# number of time steps simulated
+
+t = 0.0  				# time
+
+# Initialize the error for the simulation time
+time_error = False
+
+t_list = []	#list of the time of each simulation step
+
+
+########################################################################
+#                              PyBullet                                #
+########################################################################
+
+
+# Start the client for PyBullet
+physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+
+# Load horizontal plane
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+planeId = p.loadURDF("plane.urdf")
+
+# Set the gravity
+p.setGravity(0,0,-9.81)
+
+# Load Quadruped robot
+robotStartPos = [0,0,0.5] 
+robotStartOrientation = p.getQuaternionFromEuler([0,0,0])
+p.setAdditionalSearchPath("/opt/openrobots/share/example-robot-data/robots/solo_description/robots")
+robotId = p.loadURDF("solo.urdf",robotStartPos, robotStartOrientation)
+
+# Disable default motor control for revolute joints
+revoluteJointIndices = [0,1, 3,4, 6,7, 9,10]
+p.setJointMotorControlArray(robotId, jointIndices = revoluteJointIndices, controlMode = p.VELOCITY_CONTROL,targetVelocities = [0.0 for m in revoluteJointIndices], forces = [0.0 for m in revoluteJointIndices])
+
+""" # Initialize the joint configuration to the position straight_standing
+initial_joint_positions = [0.8, -1.6, 0.8, -1.6, -0.8, 1.6, -0.8, 1.6]
+for i in range (len(initial_joint_positions)):
+	p.resetJointState(robotId, revoluteJointIndices[i], initial_joint_positions[i])
+"""
+
+# Initialize the FR_hip configuration to 0.8
+#p.resetJointState(robotId, revoluteJointIndices[0], 0.8)
+
+# Enable torque control for revolute joints
+jointTorques = [0.0 for m in revoluteJointIndices]
+
+p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+
+# Fix the base in the world frame
+p.createConstraint(robotId, -1, -1, -1, p.JOINT_FIXED, [0, 0, 0], [0, 0, 0], [0, 0, 0.5])
+
+# Set time step for the simulation
+p.setTimeStep(dt)
+
+
+########################################################################
+#                             Simulator                                #
+########################################################################
+
+myController = controller(q0, omega, t)
+myReliefController = Relief_controller.controller()
+myEmergencyStop = EmergencyStop_controller.controller()
+
+
+for i in range (N_SIMULATION):
+	
+	time_start = time.time()
+	
+	####################################################################
+	#                 Data collection from PyBullet                    #
+	####################################################################
+	
+	jointStates = p.getJointStates(robotId, revoluteJointIndices) # State of all joints
+	
+	# Joints configuration and velocity vector
+	qmes = np.vstack((np.array([[jointStates[i_joint][0] for i_joint in range(len(jointStates))]]).T))
+	vmes = np.vstack((np.array([[jointStates[i_joint][1] for i_joint in range(len(jointStates))]]).T))
+	
+	####################################################################
+	#                Select the appropriate controller 				   #
+	#								&								   #
+	#				Load the joint torques into the robot			   #
+	####################################################################
+	
+	# If the limit bounds are reached, controller is switched to a pure derivative controller
+	if(myController.error):
+		print("Safety bounds reached. Switch to a safety controller")
+		myController = myReliefController
+		
+	# If the simulation time is too long, controller is switched to a zero torques controller
+	time_error = time_error or (time.time()-time_start > 0.003)
+	if (time_error):
+		print("Computation time lasted to long. Switch to a zero torque control")
+		myController = myEmergencyStop
+		
+	# Retrieve the joint torques from the appropriate controller
+	jointTorques = myController.control(qmes, vmes, t)
+	
+	# Time incrementation
+	t += dt
+
+	# Set control torque for all joints
+	p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+
+	# Compute one step of simulation
+	p.stepSimulation()
+
+	time_spent = time.time() - time_start
+
+	t_list.append(time_spent)
+
+
+# Plot the time of each simulation step
+plt.figure(1)
+plt.plot(t_list, 'k+')
+plt.show()
+
+p.disconnect()
